3b29f08fe0c0741b9349b15b6d9dad7c8b2bc0ec
Aviran Levi <aveloofficial@gmail.com>
2019-05-12 11:34:55 +0300

    some changes

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+
+if (process.env.PORT) { //for production enviroment 
+    console.log('production')
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    })
+} else { // for dev enviroment
+    console.log('dev')
+
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    })
+
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+        res.header('Access-Control-Allow-Credentials', true);
+        next()
+    })
+}
+
 app.listen(port, function () {
     console.log(`Running on port ${port}`)
 })
