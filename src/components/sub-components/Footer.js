edc918b2d97466c918f86d32ffe29d579bc416bc
Aviran Levi <aveloofficial@gmail.com>
2019-06-17 14:24:01 +0300

    update linkedin link

--- src/components/sub-components/Footer.js
+++ src/components/sub-components/Footer.js
@@ -1,44 +1,63 @@
-import React, { Component } from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import '../../styles/footer.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "../../styles/footer.css";
+import "react-toastify/dist/ReactToastify.css";
 
-import AlertPopup from './footer sub-components/AlertPopup';
-import { ToastContainer, toast } from 'react-toastify';
+import AlertPopup from "./footer sub-components/AlertPopup";
+import { ToastContainer, toast } from "react-toastify";
 
 class Footer extends Component {
-    openPopup = () => toast(<AlertPopup />, {
-        position: toast.POSITION.TOP_CENTER,
+  openPopup = () =>
+    toast(<AlertPopup />, {
+      position: toast.POSITION.TOP_CENTER
     });
 
-    render() {
-        return (
-            <div className="footer">
-                <ScrollAnimation className="footer-layer" animateIn="fadeIn" initiallyVisible={true}>
-                    <h1 className="footer-title">find me on social media</h1>
-                    <div className="social-media-links">
-                        <a className="linked-in social-media-link" rel="noopener noreferrer" href="https://www.linkedin.com/in/aviran-levi-29660911b/" target="_blank">
-                            <i className="fab fa-linkedin-in fa-2x"></i>
-                        </a>
+  render() {
+    return (
+      <div className="footer">
+        <ScrollAnimation
+          className="footer-layer"
+          animateIn="fadeIn"
+          initiallyVisible={true}
+        >
+          <h1 className="footer-title">find me on social media</h1>
+          <div className="social-media-links">
+            <a
+              className="linked-in social-media-link"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/aviran-levi-looking-for-my-next-challenge-29660911b/"
+              target="_blank"
+            >
+              <i className="fab fa-linkedin-in fa-2x" />
+            </a>
 
-                        <CopyToClipboard text={this.props.email}>
-                            <a className="email social-media-link" rel="noopener noreferrer" href={`mailto:${this.props.email}`} onClick={this.openPopup}>
-                                <i className="far fa-envelope fa-2x"></i>
-                            </a>
-                        </CopyToClipboard>
+            <CopyToClipboard text={this.props.email}>
+              <a
+                className="email social-media-link"
+                rel="noopener noreferrer"
+                href={`mailto:${this.props.email}`}
+                onClick={this.openPopup}
+              >
+                <i className="far fa-envelope fa-2x" />
+              </a>
+            </CopyToClipboard>
 
-                        <a className="github social-media-link" rel="noopener noreferrer" href="https://github.com/AviranLevi" target="_blank">
-                            <i className="fab fa-github fa-2x"></i>
-                        </a>
+            <a
+              className="github social-media-link"
+              rel="noopener noreferrer"
+              href="https://github.com/AviranLevi"
+              target="_blank"
+            >
+              <i className="fab fa-github fa-2x" />
+            </a>
 
-                        <ToastContainer />
-                    </div>
-                </ScrollAnimation>
-
-            </div>
-        );
-    }
+            <ToastContainer />
+          </div>
+        </ScrollAnimation>
+      </div>
+    );
+  }
 }
 
 export default Footer;
